73b4df58434964e6385dce5f0d661d6c52b26450
NakedSolidSnake <cristianosstec@gmail.com>
2025-10-28 07:39:21 -0300

    Add ip classification function

--- include/sat_network.h
+++ include/sat_network.h
@@ -4,7 +4,20 @@
 #include <sat_status.h>
 #include <stdint.h>
 
+typedef enum
+{
+    sat_network_class_multicast,
+    sat_network_class_broadcast,
+    sat_network_class_public,
+    sat_network_class_private,
+    sat_network_class_loopback,
+
+} sat_network_class_t;
+
 #define SAT_NETWORK_IP_MAX_LEN 46
+
+sat_status_t sat_network_get_ip_class (const char *const ip_address, sat_network_class_t *const ip_class);
+const char *const sat_network_get_ip_class_string (const sat_network_class_t ip_class);
 
 sat_status_t sat_network_get_public_ip (const char *const dns_address, uint16_t dns_port, char ip_address [SAT_NETWORK_IP_MAX_LEN + 1]);
 
