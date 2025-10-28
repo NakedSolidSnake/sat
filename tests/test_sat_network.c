ae2a23e092278aee2ce6bbb296986fb9a64ff26d
NakedSolidSnake <cristianosstec@gmail.com>
2025-10-28 07:24:39 -0300

    Create sat network

--- tests/test_sat_network.c
+++ tests/test_sat_network.c
@@ -10,6 +10,7 @@ int main (void)
     sat_status_t status = sat_network_get_public_ip (NULL, 0, ip_address);
 
     assert (sat_status_get_result (&status) == true);
+<<<<<<< HEAD
 
     sat_network_class_t ip_class;
 
@@ -118,6 +119,10 @@ int main (void)
     {
         printf ("IPv6 connectivity not available or failed\n");
     }
+=======
+    
+    printf ("Public IP Address: %s\n", ip_address);
+>>>>>>> c0257ca (Create sat network)
 
     return 0;
 }
