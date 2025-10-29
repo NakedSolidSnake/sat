4833092d818b4a57bb47d6937d0258f1bda15c00
NakedSolidSnake <cristianosstec@gmail.com>
2025-10-29 07:21:09 -0300

    add new functions

--- include/sat_uuid.h
+++ include/sat_uuid.h
@@ -3,12 +3,22 @@
 
 #include <stdbool.h>
 
+
+#define SAT_UUID_STRING_SIZE 37
+#define SAT_UUID_BINARY_SIZE 16
+
+typedef unsigned char uuid_binary_t [SAT_UUID_BINARY_SIZE];
+typedef char uuid_string_t [SAT_UUID_STRING_SIZE];
+
 typedef enum 
 {
     sat_uuid_format_lower_case,
     sat_uuid_format_upper_case
 } sat_uuid_format_t;
 
-bool sat_uuid_generate (char uuid[37], sat_uuid_format_t format);
+bool sat_uuid_generate_string (uuid_string_t uuid_string, sat_uuid_format_t format);
+bool sat_uuid_generate_bin (uuid_binary_t uuid_bin);
+bool sat_uuid_string_to_bin (const uuid_string_t uuid_string, uuid_binary_t uuid);
+bool sat_uuid_bin_to_string (const uuid_binary_t uuid_bin, uuid_string_t uuid_string, sat_uuid_format_t format);
 
-#endif/* SAT_UUID_H */
+#endif /* SAT_UUID_H */
