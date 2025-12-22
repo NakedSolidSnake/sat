d8d824cded5b88dff665a9e2fa8ae2ad843ee87e
NakedSolidSnake <cristianosstec@gmail.com>
2025-12-22 09:00:16 -0300

    Change sat_loki to a module

--- cmake/CMakeLokiDependency.cmake
+++ cmake/CMakeLokiDependency.cmake
@@ -2,15 +2,4 @@ option (SAT_LOKI "SAT_LOKI support" OFF)
 
 if (${SAT_LOKI})
     message (STATUS "SAT_LOKI support enable")
-
-    if (NOT SAT_CURL)
-        message (FATAL_ERROR "SAT_LOKI requires SAT_CURL to be enabled. Please enable SAT_CURL first.")
-    endif()
-
-    # check_library (CIVETWEB "civetweb")
-
-    list (APPEND SAT_SOURCES ${CMAKE_SOURCE_DIR}/src/sat_loki.c)
-    file (APPEND ${CMAKE_SOURCE_DIR}/include/sat_optionals.h "#include <sat_loki.h>\n")
-
-    install (FILES include/sat_loki.h DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
 endif ()
--- include/sat_optionals.h
+++ include/sat_optionals.h
@@ -1,3 +1,4 @@
 #include <sat_curl.h>
 #include <sat_json.h>
 #include <sat_webserver.h>
+#include <sat_loki.h>
