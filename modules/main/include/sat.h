62d3d31f380ad41c7821f216f9039ae0a920e934
NakedSolidSnake <cristianosstec@gmail.com>
2025-12-23 20:46:52 -0300

    add builtin header

--- modules/main/include/sat.h
+++ modules/main/include/sat.h
@@ -1,36 +1,7 @@
 #ifndef SAT_H
 #define SAT_H
 
-#include <sat_uuid.h>
-#include <sat_file.h>
-#include <sat_time.h>
-#include <sat_status.h>
-#include <sat_log.h>
-#include <sat_validation.h>
-#include <sat_event.h>
-#include <sat_stack.h>
-#include <sat_array.h>
-#include <sat_udp.h>
-#include <sat_linked_list.h>
-#include <sat_queue.h>
-#include <sat_map.h>
-#include <sat_tcp.h>
-#include <sat_worker.h>
-#include <sat_set.h>
-#include <sat_plugin.h>
-#include <sat_cache.h>
-#include <sat_scheduler.h>
-#include <sat_channel.h>
-#include <sat_shared_memory.h>
-#include <sat_queue_ipc.h>
-#include <sat_iterator.h>
-#include <sat_process.h>
-#include <sat_process_monitor.h>
-#include <sat_network.h>
-#include <sat_discovery.h>
-#include <sat_network.h>
-#include <sat_directory.h>
-
+#include <sat_builtin.h>
 #include <sat_optionals.h>
 
 #endif/* SAT_H */
