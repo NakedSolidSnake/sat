0a7fa31ab1182931e4d8662f6a69ae12d15b59e1
NakedSolidSnake <cristianosstec@gmail.com>
2026-05-01 17:06:32 -0300

    Refactor sat_event

--- modules/builtin/sat_event/lib/src/sat_event.c
+++ modules/builtin/sat_event/lib/src/sat_event.c
@@ -3,53 +3,41 @@
 
 sat_status_t sat_event_init (sat_event_dispatcher_t *const object)
 {
-    sat_status_t status = sat_status_set (&status, false, __func__, "sat event init error");
+    sat_status_return_on_null (object, "object pointer is NULL");
 
-    if (object != NULL)
-    {
-        memset (object, 0, sizeof (sat_event_dispatcher_t));
-        sat_status_set (&status, true, __func__, "");
-    }
+    memset (object, 0, sizeof (sat_event_dispatcher_t));
 
-    return status;
+    sat_status_return_on_success ();
 }
 
 sat_status_t sat_event_observer_add (sat_event_dispatcher_t *const object, sat_event_t event, const sat_event_observer_t *const observer)
 {
-    sat_status_t status = sat_status_set (&status, false, __func__, "sat event observer add error");
+    sat_status_return_on_null (object, "object pointer is NULL");
+    sat_status_return_on_null (observer, "observer pointer is NULL");
+    sat_status_return_on_equals (object->amount, SAT_EVENT_OBSERVER_AMOUNT, "observer limit reached");
 
-    if (object != NULL && observer != NULL && object->amount < SAT_EVENT_OBSERVER_AMOUNT)
-    {
-        object->map [object->amount].event = event;
-        object->map [object->amount].observer = observer;
-        object->map [object->amount].context = (void *)observer;
+    object->map [object->amount].event = event;
+    object->map [object->amount].observer = observer;
+    object->map [object->amount].context = (void *)observer;
 
-        object->amount ++;
+    object->amount ++;
 
-        sat_status_set (&status, true, __func__, "");
-    }
-
-    return status;
+    sat_status_return_on_success ();
 }
 
 sat_status_t sat_event_fire (const sat_event_dispatcher_t *const object, sat_event_t event, const void *const data)
 {
-    sat_status_t status = sat_status_set (&status, false, __func__, "sat event fire error");
+    sat_status_return_on_null (object, "object pointer is NULL");
 
-    if (object != NULL)
+    for (uint8_t i = 0; i < object->amount; i++)
     {
+        const sat_event_observer_map_t *const map = &object->map [i];
 
-        for (uint8_t i = 0; i < object->amount; i++)
+        if (event == map->event)
         {
-            const sat_event_observer_map_t *const map = &object->map [i];
-
-            if (event == map->event)
-            {
-                map->observer->base.handler (map->context, data);
-                sat_status_set (&status, true, __func__, "");
-            }
-        }        
+            map->observer->base.handler (map->context, data);
+        }
     }
 
-    return status;
+    sat_status_return_on_success ();
 }
